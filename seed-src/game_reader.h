6e0fc201c152bd72aab01de021dc4c24f5b2d9e7
Saúl <134432844+TheOneCalledLuas@users.noreply.github.com>
2025-01-29 21:02:10 +0100

    Game_reader creation v1.0
    Added the files and started working at them.

---
+++ seed-src/game_reader.h
@@ -0,0 +1,19 @@
+/**
+ * @brief It loads all the game data into the program.
+ *
+ * @file game_reader.h
+ * @author Saúl López Romero
+ * @version 0
+ * @date 29-01-2025
+ * @copyright GNU Public License
+ */
+
+#ifndef GAME_READER_H
+#define GAME_READER_H
+
+
+
+
+
+
+#endif
