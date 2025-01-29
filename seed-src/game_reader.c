06cd5e31b06873844cd9f3f243614cf516944bdd
Saúl <134432844+TheOneCalledLuas@users.noreply.github.com>
2025-01-29 21:29:17 +0100

    Added a makefile, fixed .gitignore

--- seed-src/game_reader.c
+++ seed-src/game_reader.c
@@ -1,2 +1,8 @@
 #include "game_reader.h"
+#include "game.h"
 
+Status game_load_spaces(Game *game, char *filename);
+
+Status game_add_space(Game *game, Space *space);
+
+Id game_get_space_id_at(Game *game, int position);
