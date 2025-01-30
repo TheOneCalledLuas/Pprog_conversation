b4ebdb357f7c54ab6f7dc006b723593d209a365a
Saúl <134432844+TheOneCalledLuas@users.noreply.github.com>
2025-01-30 21:59:35 +0100

    Documented game_reader to 50%

--- seed-src/game_reader.c
+++ seed-src/game_reader.c
@@ -19,19 +19,28 @@ Space *game_get_space(Game *game, Id id)
 {
     int i = 0;
 
+    /*Checks the pointer.*/
+    if (!game)
+    {
+        return NULL;
+    }
+
+    /*Validates the id.*/
     if (id == NO_ID)
     {
         return NULL;
     }
 
+    /*Searchs for the space.*/
     for (i = 0; i < game->n_spaces; i++)
     {
         if (id == space_get_id(game->spaces[i]))
         {
+            /*Returns the space when found.*/
             return game->spaces[i];
         }
     }
-
+    /*The space wasn't found.*/
     return NULL;
 }
 
@@ -45,21 +54,26 @@ Status game_load_spaces(Game *game, char *filename)
     Space *space = NULL;
     Status status = OK;
 
-    if (!filename)
+    /*Checks the pointers.*/
+    if (!filename || !game)
     {
         return ERROR;
     }
 
+    /*Opens the file.*/
     file = fopen(filename, "r");
     if (file == NULL)
     {
         return ERROR;
     }
 
+    /*Gets the data lie by line.*/
     while (fgets(line, WORD_SIZE, file))
     {
+        /*Checks that the line contains a room.*/
         if (strncmp("#s:", line, 3) == 0)
         {
+            /*Takes the information data by data and saves it.*/
             toks = strtok(line + 3, "|");
             id = atol(toks);
             toks = strtok(NULL, "|");
@@ -72,9 +86,11 @@ Status game_load_spaces(Game *game, char *filename)
             south = atol(toks);
             toks = strtok(NULL, "|");
             west = atol(toks);
+/*If DEBUG mode is active (defined) prints what it has read.*/
 #ifdef DEBUG
             printf("Leido: %ld|%s|%ld|%ld|%ld|%ld\n", id, name, north, east, south, west);
 #endif
+            /*Creates a new space and fills it with the information gathered.*/
             space = space_create(id);
             if (space != NULL)
             {
@@ -87,14 +103,15 @@ Status game_load_spaces(Game *game, char *filename)
             }
         }
     }
-
+    /*If something went wrong while reading the file.*/
     if (ferror(file))
     {
         status = ERROR;
     }
-
+    /*Closes the file.*/
     fclose(file);
 
+    /*Clean exit.*/
     return status;
 }
 
--- seed-src/game_reader.h
+++ seed-src/game_reader.h
@@ -17,13 +17,21 @@
 /**
  * @brief locates an space by its id.
  * @author Saúl López Romero
- * 
+ *
  * @param game game structure, where all the information related to game is included.
  * @param id id of the space
  * @return pointer to the space, if it exists.
  */
 Space *game_get_space(Game *game, Id id);
 
+/**
+ * @brief loads all the spaces from the data file.
+ * @author Saúl López Romero
+ *
+ * @param game game structure, where all the information related to game is included.
+ * @param filename name of the data file where the map is stored.
+ * @return OK for a clean exit, otherwise ERROR.
+ */
 Status game_load_spaces(Game *game, char *filename);
 
 Status game_add_space(Game *game, Space *space);
