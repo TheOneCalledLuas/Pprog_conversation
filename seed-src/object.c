70ec08d5198144fecc23375a650db829d36dd1d6
Saúl <134432844+TheOneCalledLuas@users.noreply.github.com>
2025-02-01 19:08:25 +0100

    Object module creation and basic funcs (geters and seters)

---
+++ seed-src/object.c
@@ -0,0 +1,109 @@
+/**
+ * @brief It implements the object functionality.
+ *
+ * @file object.c
+ * @author Saúl López Romero
+ * @version 0
+ * @date 01-02-2025
+ * @copyright GNU Public License
+ */
+
+#include "object.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * @brief Object
+ *
+ * Object structure with all the relevant information regarding the object.
+ */
+struct _Object
+{
+    Id id;                    /* Id of the object, it must be unique. */
+    char name[WORD_SIZE + 1]; /* Object name. */
+};
+
+Object *object_create(Id id)
+{
+    Object *object = NULL;
+
+    /*Checks that the id is at least valid.*/
+    if (id <= 0)
+    {
+        return NULL;
+    }
+
+    /*Allocates memory and checks if it goes alright.*/
+    object = (Object *)malloc(sizeof(Object));
+    if (!object)
+    {
+        return NULL;
+    }
+
+    /*Sets the vaues to default ones and establishes the id.*/
+    object->id = id;
+    object->name[0] = '\0';
+
+    /*Clean exit.*/
+    return object;
+}
+
+Status object_destroy(Object *object)
+{
+
+    /*Checks the arguments.*/
+    if (!object)
+    {
+        return ERROR;
+    };
+
+    /*Frees the memory.*/
+    free(object);
+
+    /*Clean exit.*/
+    return OK;
+}
+
+char *object_get_name(Object *object)
+{
+    /*Checks the arguments.*/
+    if (!object)
+    {
+        return ERROR;
+    }
+
+    /*returns a pointer to the object's name.*/
+    return object->name;
+}
+
+Status object_set_name(Object *object, char *name)
+{
+    /*Checks the arguments.*/
+    if (!object || !name)
+    {
+        return ERROR;
+    }
+
+    /*Sets the name.*/
+    if (!(strcpy(object->name, name)))
+    {
+        return ERROR;
+    }
+
+    /*Clean exit.*/
+    return OK;
+}
+
+Id object_get_id(Object *object)
+{
+    /*Checks the arguments.*/
+    if (!object)
+    {
+        return NO_ID;
+    }
+
+    /*Returns the id.*/
+    return object->id;
+}
--- seed-src/object.h
+++ seed-src/object.h
@@ -1,5 +1,5 @@
 /**
- * @brief It handles object management.
+ * @brief It handles object related tasks.
  *
  * @file object.h
  * @author Saúl López Romero
@@ -13,8 +13,52 @@
 
 #include "types.h"
 
+typedef struct _Object Object;
 
+/**
+ * @brief It handles all the object creation proccess.
+ * @author Saúl López Romero
+ * 
+ * @param id Unique id of the object structure. (make sure its not repeated)
+ * @return Pointer to the object.
+ */
+Object * object_create(Id id);
 
+/**
+ * @brief Destroys the object structure propperly.
+ * @author Saúl López Romero
+ * 
+ * @param object Pointer to the object.
+ * @return OK if it works as intended, ERROR otherwise.
+ */
+Status object_destroy(Object *object);
+
+/**
+ * @brief Gets the object's name.
+ * @author Saúl López Romero
+ * 
+ * @param object Pointer to the object.
+ * @return Pointer to the object name.
+ */
+char *object_get_name(Object *object);
+
+/**
+ * @brief Sets the object's name.
+ * @author Saúl López Romero
+ * 
+ * @param object Pointer to the object.
+ * @return OK if it works as intended, ERROR otherwise.
+ */
+Status object_set_name(Object *object, char *name);
+
+/**
+ * @brief Gets the object's name.
+ * @author Saúl López Romero
+ * 
+ * @param object Pointer to the object.
+ * @return Object's id.
+ */
+Id object_get_id(Object *object);
 
 
 #endif
