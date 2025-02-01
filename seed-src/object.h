dc0881bfd1d95a28836048e9454c42c52342b1b0
Saúl <134432844+TheOneCalledLuas@users.noreply.github.com>
2025-02-01 18:11:42 +0100

    Created object files and edited makefile for compatibility

---
+++ seed-src/object.h
@@ -0,0 +1,20 @@
+/**
+ * @brief It handles object management.
+ *
+ * @file object.h
+ * @author Saúl López Romero
+ * @version 0
+ * @date 2-02-2025
+ * @copyright GNU Public License
+ */
+
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include "types.h"
+
+
+
+
+
+#endif
