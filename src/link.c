2d163938d9d6365ad31b45ced015810ab8138160
Nerinne <ire.3n3r1@gmail.com>
2025-04-07 10:22:01 +0200

    semiapropiación indebida 5.(debida por gant :3)

--- src/link.c
+++ src/link.c
@@ -2,7 +2,7 @@
  * @brief It implements the link module
  *
  * @file link.c
- * @author Fernando Mijangos
+ * @author Fernando Mijangos, Irene García
  * @version 1
  * @date 21-03-2025
  * @copyright GNU Public License
