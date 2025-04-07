09c9a789c794915b5da487cc90400473a07a764e
Nerinne <ire.3n3r1@gmail.com>
2025-04-07 10:23:26 +0200

    Por qué pones todos tus apellidos

--- src/command.c
+++ src/command.c
@@ -2,7 +2,7 @@
  * @brief It implements the command interpreter
  *
  * @file command.c
- * @author Fernando Mijangos, Saul Lopez Romero, Raquen Anguita
+ * @author Fernando Mijangos, Saul Lopez Romero, Raquen Anguita Martínez de Velasco
  * @version 3
  * @date 29-02-2025
  * @copyright GNU Public License
