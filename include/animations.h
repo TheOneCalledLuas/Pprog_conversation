938d6ddfc7615db2f05f8166eb5eff9d70cc02fc
Saúl <134432844+TheOneCalledLuas@users.noreply.github.com>
2025-04-14 19:14:19 +0100

    READ DESC One step nearer to the animations (But not close)
    Subo este commit para que la gente que tenga en mente usar animaciones pueda ver la estrucutra de estas, o por lo menos la idea de como quiero que sean, y poder así empezar a adaptar las ideas que tengamos, concretando así un poco más el proyecto

---
+++ include/animations.h
@@ -0,0 +1,22 @@
+/**
+ * @brief It implements animations into the game.
+ *
+ * @file animations.h
+ * @author Saúl López Romero
+ * @version 1
+ * @date 14-04-2025
+ * @copyright GNU Public License
+ */
+
+#ifndef ANIMATIONS_H
+#define ANIMATIONS_H /*!< Animations lib.*/
+
+
+#define MAX_ANIMATIONS 100 /*!< Max animation number.*/
+
+typedef struct _Animation_Manager Animation_Manager; /*!< Animation manager type.*/
+typedef struct _Animation Animation; /*!< Animation type.*/
+
+
+
+#endif
---
+++ src/animations.c
@@ -0,0 +1,43 @@
+/**
+ * @brief It implements animations into the game.
+ *
+ * @file animations.c
+ * @author Saúl López Romero
+ * @version 1
+ * @date 14-04-2025
+ * @copyright GNU Public License
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "animations.h"
+#include "types.h"
+
+#define MAX_LINE 128 /*!< Max nimber of cheracters per line.*/
+
+/**
+ * @brief This struct is in charge of handling all the game animations.
+ */
+struct _Animation_Manager
+{
+    Animation *animations;           /*!< Animation list.*/
+    int n_animations;                /*!< Number of animations.*/
+    char animations_route[MAX_LINE]; /* Path to the animation dir.*/
+};
+
+/**
+ * @brief Animation stucture.
+ */
+struct _Animation
+{
+    Id id;                /*!< Animation id.*/
+    char *animation_file; /*!< Animation file, where the data is stored.*/
+    long n_images;        /*!< Number of frames for the animation.*/
+    int widht;            /*!< Animation widht.*/
+    int lenght;           /*!< Animation lenght.*/
+    int side_padding;     /*!< Number of white spaces left to the left, set to zero by default.*/
+    int height_padding;   /*!< Number of white spaces left bellow the animation, set to zero by default.*/
+    float refresh_rate;   /*!< Seconds left between each frame.*/
+};
