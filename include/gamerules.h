c23703ea11ffe4fb609268648fe3ef567570e781
Saúl <134432844+TheOneCalledLuas@users.noreply.github.com>
2025-04-20 20:36:01 +0100

    Gamerules module is now fully functional, yet to be implemented though

--- include/gamerules.h
+++ include/gamerules.h
@@ -12,9 +12,30 @@
 #define GAMERULES_H /*!< Gamerules lib.*/
 
 typedef struct _Game_values Game_values; /*!< Struct with all the game values, which determine some outcomes.*/
-typedef struct _Gamerule Gamerule; /*!< Gamrule struct.*/
+typedef struct _Gamerule Gamerule;       /*!< Gamerule struct.*/
 
-typedef Status (*Gamerule_func)(Game *game, Gamerule*gr); /*!< Pointer to function stored in a Gamerule.*/
+typedef Status (*Gamerule_func)(Game *game, Gamerule *gr); /*!< Pointer to function stored in a Gamerule.*/
 
+Game_values *gamerules_values_init();
+void gamerules_values_destroy(Game_values *gv);
+int gamerules_get_n_gamerules(Game_values *gv);
+Status gamerules_values_add(Game_values *gv, Gamerule *gr);
+Gamerule *gamerules_values_delete_last(Game_values *gv);
+Gamerule *gamerules_gamerule_create(Id id);
+void gamerules_gamerule_destroy(Gamerule *gr);
+Status gamerules_gamerule_set_func(Gamerule *gr, Gamerule_func g_func);
+Gamerule_func gamerules_gamerule_get_func(Gamerule *gr);
+Status gamerule_try_exec(Game *game, Gamerule *gr);
+Id gamerules_get_id(Gamerule *gr);
+char *gamerules_get_name(Gamerule *gr);
+Bool gamerules_get_valid(Gamerule *gr);
+int gamerules_get_n_exec_times(Gamerule *gr);
+Bool gamerules_get_do_once(Gamerule *gr);
+int gamerules_get_value(Gamerule *gr);
+Status gamerules_set_name(Gamerule *gr, char *name);
+Status gamerules_set_valid(Gamerule *gr, Bool value);
+int gamerules_increment_has_exec(Gamerule *gr);
+Status gamerules_set_do_once(Gamerule *gr, Bool value);
+Status gamerules_set_value(Gamerule *gr, int value);
 
 #endif
--- src/gamerules.c
+++ src/gamerules.c
@@ -9,6 +9,11 @@
  */
 
 #define MAX_GAMERULES 100 /*!< Max gamerule number. Can be modified if required.*/
+#define MAX_NAME 64       /*!< Max name for a gamerule.*/
+
+/* Por lo que sea vscode no se traga que esta linea estae n el .h, la pongo y la quitaré al final
+para que quite las rayas rojas de una vez.*/
+typedef Status (*Gamerule_func)(Game *game, Gamerule *gr); /*!< Pointer to function stored in a Gamerule.*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,6 +24,7 @@
 
 struct _Gamerule
 {
+    Id id;                     /*!< Gamerule id. Must be unique.*/
     char *name;                /*!< Gamerule name.*/
     Bool is_valid;             /*!< Value used to turn the gamerule on or off. */
     int has_exec;              /*!< Number of times a gamerule has been executed.*/
@@ -64,3 +70,223 @@ void gamerules_values_destroy(Game_values *gv)
     }
 }
 
+int gamerules_get_n_gamerules(Game_values *gv)
+{
+    return (gv ? gv->n_gamerules : -1);
+}
+
+Status gamerules_values_add(Game_values *gv, Gamerule *gr)
+{
+    /*Error handling.*/
+    if (!gv || !gr || gv->n_gamerules >= MAX_GAMERULES)
+    {
+        return ERROR;
+    }
+    /*Adds the gamerule.*/
+    gv->gamerules[gv->n_gamerules++] = gr;
+
+    /*Clean exit.*/
+    return OK;
+}
+
+Gamerule *gamerules_values_delete_last(Game_values *gv)
+{
+    /*Error handling.*/
+    if (!gv || gv->n_gamerules <= 0)
+        return NULL;
+
+    /*Takes the item out.*/
+    return gv->gamerules[--(gv->n_gamerules)];
+}
+
+Gamerule *gamerules_gamerule_create(Id id)
+{
+    Gamerule *gr = NULL;
+    /*Error handling.*/
+    if (id <= NO_ID)
+        return NULL;
+
+    /*Memory allocation.*/
+    if (!(gr = (Gamerule *)calloc(1, sizeof(Gamerule))))
+        return NULL;
+
+    if (!(gr->name = (char *)calloc(MAX_NAME, sizeof(char))))
+    {
+        free(gr);
+        return NULL;
+    }
+
+    /*Sets the values to default.*/
+    gr->do_once = FALSE;
+    gr->has_exec = FALSE;
+    gr->id = id;
+    gr->is_valid = FALSE;
+    gr->name[0] = '\0';
+    gr->value = 0;
+
+    /*Clean exit.*/
+    return gr;
+}
+
+void gamerules_gamerule_destroy(Gamerule *gr)
+{
+    /*Error handling.*/
+    if (gr)
+    {
+        if (gr->name)
+        {
+            free(gr->name);
+        }
+        free(gr);
+    }
+    return;
+}
+
+Status gamerules_gamerule_set_func(Gamerule *gr, Gamerule_func g_func)
+{
+    /*Error handling.*/
+    if (!gr || !g_func)
+        return ERROR;
+
+    /*Sets the value.*/
+    gr->do_gamerule = g_func;
+    return OK;
+}
+
+Gamerule_func gamerules_gamerule_get_func(Gamerule *gr)
+{
+    /*Error handling.*/
+    if (!gr)
+        return NULL;
+
+    /*Returns the value.*/
+    return gr->do_gamerule;
+}
+
+Status gamerule_try_exec(Game *game, Gamerule *gr)
+{
+    /*Error handling.*/
+    if (!game || !gr)
+        return ERROR;
+
+    /*Checks if the gamerule meets the requirements to be executed.*/
+    if (gr->is_valid == TRUE)
+    {
+        switch (gr->do_once)
+        {
+        case FALSE:
+            return (gr->do_gamerule)(game, gr);
+            break;
+
+        case TRUE:
+            if (gr->has_exec == 0) /*If its the first time the gamerule is activated.*/
+            {
+                return (gr->do_gamerule)(game, gr);
+            }
+            else
+            {
+                return OK; /*The gamerule doesn't need to be executed.*/
+            }
+            break;
+
+        default:
+            return ERROR;
+            break;
+        }
+    }
+}
+
+Id gamerules_get_id(Gamerule *gr)
+{
+    return (gr ? gr->id : ID_ERROR);
+}
+
+char *gamerules_get_name(Gamerule *gr)
+{
+    return (gr ? gr->name : NULL);
+}
+
+Bool gamerules_get_valid(Gamerule *gr)
+{
+    return (gr ? gr->is_valid : FALSE);
+}
+
+int gamerules_get_n_exec_times(Gamerule *gr)
+{
+    return (gr ? gr->has_exec : -1);
+}
+
+Bool gamerules_get_do_once(Gamerule *gr)
+{
+    return (gr ? gr->do_once : FALSE);
+}
+
+int gamerules_get_value(Gamerule *gr)
+{
+    return (gr ? gr->value : 0);
+}
+
+Status gamerules_set_name(Gamerule *gr, char *name)
+{
+    /*Error handling.*/
+    if (!gr || !name || strlen(name) >= MAX_NAME)
+        return ERROR;
+
+    /*Sets the value.*/
+    strcpy(gr->name, name);
+
+    /*Clean exit.*/
+    return OK;
+}
+
+Status gamerules_set_valid(Gamerule *gr, Bool value)
+{
+    /*Error handling.*/
+    if (!gr)
+        return ERROR;
+
+    /*Sets the value.*/
+    gr->is_valid = value;
+
+    /*Clean exit.*/
+    return OK;
+}
+
+int gamerules_increment_has_exec(Gamerule *gr)
+{
+    /*Error handling.*/
+    if (!gr)
+        return ERROR;
+
+    /*Sets the value.*/
+    gr->has_exec++;
+
+    /*Clean exit.*/
+    return OK;
+}
+
+Status gamerules_set_do_once(Gamerule *gr, Bool value)
+{
+    /*Error handling.*/
+    if (!gr)
+        return ERROR;
+
+    /*Sets the value.*/
+    gr->do_once = value;
+
+    /*Clean exit.*/
+    return OK;
+}
+
+Status gamerules_set_value(Gamerule *gr, int value)
+{
+    /*Error handling.*/
+    if (!gr)
+        return ERROR;
+
+    /*Sets the value.*/
+    gr->value = value;
+
+    /*Clean exit.*/
+    return OK;
+}
