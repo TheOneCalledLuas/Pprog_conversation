d2f3ae398ab1d702541dcd59c27ea4e9bcb2cbc7
Fernandpoo <2040fer@gmail.com>
2025-05-11 19:19:49 +0200

    Passing some things, only the ones that help

---
+++ Test_all_things.cmd
@@ -0,0 +1,86 @@
+1
+test_all
+menu
+1
+4
+2
+4
+3
+4
+4
+m e
+m e
+i wooden_sign
+i WRONG_NAME
+m 
+m u
+m d
+m d
+m u
+m n
+m s
+m s
+m n
+m e
+m e
+i Stone_sign
+t mushroom
+d mushroom
+t mushroom
+m e
+m e
+i ancient_sign
+m e
+m e
+i dirty_sign
+a weak_spider
+m e
+m e
+use mushroom
+use rock over spidey
+i bloody_sign
+m s
+m s
+c Trantveler
+i Wet_sign
+r Trantveler
+m s
+m s
+i new_sign
+open wood_door with wood_key
+m e
+m e
+i rusty_sign
+f Trantveler
+m e
+m e
+t train_pass
+m e
+m e
+i Antrain
+use train_pass over Antrain
+m s
+m s
+coop anthony
+c Merchant
+r Merchant
+m e
+a spider_guard
+a spider_guard
+a spider_guard
+a spider_guard
+a spider_guard
+a spider_guard
+a spider_guard
+a spider_guard
+a spider_guard
+a spider_guard
+a spider_guard
+m u
+c worried_ant
+m s
+i treasure
+m e
+m e
+
+
